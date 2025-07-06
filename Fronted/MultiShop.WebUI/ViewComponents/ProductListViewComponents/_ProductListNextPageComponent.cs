adabe589db6d11f2f14493afa031aa442b65c2a2
yiğitcan tunay <versen_56@outlook.com>
2025-07-06 23:44:39 +0300

    Güncel Repo

---
+++ Fronted/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListNextPageComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListNextPageComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Fronted/MultiShop.WebUI/ViewComponents/ShoppingCardViewComponents/_ShoppingCardProductListComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCardViewComponents
+{
+    public class _ShoppingCardProductListComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }
+    }
+}
